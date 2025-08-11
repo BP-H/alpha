fe32c754cdf692606c7d716d57911c4f03d9c272
BP-H <blueprinthustler@gmail.com>
2025-08-11 18:09:36 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    // safe if you later switch <img> -> <Image> for picsum
     remotePatterns: [{ protocol: 'https', hostname: 'picsum.photos' }],
   },
 };
